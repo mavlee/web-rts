2632192dbb01fce73420626db9dd02eefd7eaea1
Maverick <maverick.lee225@gmail.com>
2012-04-25 21:17:28 -0400

    Socket IO testing

--- apptest.js
+++ apptest.js
@@ -1,8 +1,9 @@
 var io = require('socket.io').listen(8080);
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-      socket.on('my other event', function (data) {
-            console.log(data);
-              });
+    setInterval(function() { socket.emit('cycle', {"hi": "bye"}); }, 2000);
+    //socket.emit('news', { hello: 'world' });
+    socket.on('test', function (data) {
+      console.log(data);
+    });
 });
